e5976ee5e319511e4c28c45f5c5fcbd297c4e853
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-21 10:47:29 -0400

    created models title body datetime.import datetime

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class   Post(models.Model):
+    title = models.CharFied(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
