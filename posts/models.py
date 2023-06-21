614c2459c2d786d0a0cadbf84923db2ce3f7330a
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-21 10:53:30 -0400

    corrected CharField sp models.py ln5

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 class   Post(models.Model):
-    title = models.CharFied(max_length=100)
+    title = models.CharField(max_length=100)
     body = models.CharField(max_length=1000000)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
