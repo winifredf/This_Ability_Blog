72e78cba3ba81dc520d6747aaf72c73f0277e60f
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-22 10:27:32 -0400

    created post function in views to render posts

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,6 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def post(request):
+    return render(request, 'post.html')
