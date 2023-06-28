15d41440bed7b0bed042fadda77025d99a724df5
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-27 20:50:01 -0400

    created login/logout functions in views.

--- posts/views.py
+++ posts/views.py
@@ -8,3 +8,10 @@ def index(request):
 
 def register(request):
     return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
