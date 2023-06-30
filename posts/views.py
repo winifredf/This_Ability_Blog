85f7991404ecbfb946c233c0b31b68a7b26909f5
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-30 17:49:49 -0400

    created login, logout, signup functions in views

--- posts/views.py
+++ posts/views.py
@@ -6,18 +6,31 @@ def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
 
-def template(request):
-    return render(request, 'template.html')
-
-    def register(request):
-        return render(request, 'register.html')
-
 def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def signup(request):
+    return render(request,'signup.html')
+#def login(request):
     if  "username" == request.POST.get('username') and "password" == request.POST.get('password'):
         return render(request, 'login.html')
     else: return ('User could not be authenticated. Please try again.')
 
-def logout(request):
+#def logout(request):
     logout(request)
     return render(request, 'index.html')
+
+#def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return render(request, 'login.html')
+    else:
+        return render(request,'signup.html')
 
