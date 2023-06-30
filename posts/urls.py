f0d0b47306565f385cedb2e0d19bbc92a7c40d12
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-30 18:36:17 -0400

    removed unneeded posts import

--- posts/urls.py
+++ posts/urls.py
@@ -2,17 +2,13 @@ from pipes import Template
 from django import template, templatetags
 from django.urls import path
 from . import views
-#from django.urls import include
 
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('post', views.post,),
     path('post/<str:pk>', views.post, name='post'),
     path('login', views.login),
     path('logout', views.logout),
     path('signup', views.signup),
-
-    path('register', template.register, name='register'),
-    path('login', templatetags.login.html, name='login'),
-    path('logout', Template.logout, name='logout'),
     ]
