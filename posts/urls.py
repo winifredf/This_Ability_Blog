15eca558fd08c943ecc3b1f8f749bb0b87e90d1c
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-30 17:57:11 -0400

    updated url path login, logout, signup

--- posts/urls.py
+++ posts/urls.py
@@ -2,12 +2,16 @@ from pipes import Template
 from django import template, templatetags
 from django.urls import path
 from . import views
-from django.urls import include
+#from django.urls import include
 
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('post/<str:pk>', views.post, name='post'),
+    path('login', views.login),
+    path('logout', views.logout),
+    path('signup', views.signup),
+
     path('register', template.register, name='register'),
     path('login', templatetags.login.html, name='login'),
     path('logout', Template.logout, name='logout'),
