4c0640c851afa263ab674d18f176ced65f788778
Winifredf <Winifred.Dykes@hotmail.com>
2023-06-30 21:20:52 -0400

    added admin and include paths and import

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,14 @@
 from pipes import Template
 from django import template, templatetags
 from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')) 
     path('', views.index, name= 'index'),
     path('/post', views.post,),
     path('post/<str:pk>', views.post, name='post'),
